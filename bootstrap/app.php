a856c4753199899ccaf67e68780347285464d782
dsilva <sfadiego@gmail.com>
2024-09-03 17:03:58 -0600

    bootstrap, config, public

---
+++ bootstrap/app.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Response;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {})
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (ValidationException $e) {
+            return Response::json([
+                'success' => false,
+                'data' => $e->errors(),
+            ]);
+        });
+    })->create();
