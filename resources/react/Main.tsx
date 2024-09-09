c2a9cde673b6e27508b65924278ea7befd8d701d
dsilva <sfadiego@gmail.com>
2024-09-09 12:56:07 -0600

    cambios en estructura para front end

--- resources/react/Main.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Main extends Component {
-    render() {
-        return (
-            <div>Welcome react version </div>
-        )
-    }
-}
