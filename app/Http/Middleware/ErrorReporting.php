9ed0257e6f1a66ee21610f60fd18191111dac628
Diego Silva <sfadiego@gmail.com>
2026-02-12 21:49:06 -0600

    refactor respuestas api

---
+++ app/Http/Middleware/ErrorReporting.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\ErrorReporting as ModelsErrorReporting;
+use App\Traits\BackupDatabase;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class ErrorReporting
+{
+    use BackupDatabase; // TODO: implementar correctamente con docker
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        if ($response->getStatusCode() == 500) {
+            Log::error('Fatal Error', [
+                'endpoint' => $request->path(),
+                'method' => $request->method(),
+                'error_message' => $response->exception?->getMessage() ?? 'Unknown error',
+            ]);
+        }
+
+        if ($response->getStatusCode() > 400) {
+            ModelsErrorReporting::create([
+                'endpoint' => $request->path(),
+                'method' => $request->method(),
+                'status_code' => $response->getStatusCode(),
+                'error_message' => $response->exception?->getMessage() ?? 'Unknown error',
+                'request_payload' => $request->all(),
+                'response_body' => $response->getContent(),
+            ]);
+        }
+
+        return $response;
+    }
+}
--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -27,8 +27,9 @@ class CategoriesSeeder extends Seeder
             'EXTRAS',
         ];
 
-        foreach ($categories as $category) {
-            CategoryModel::updateOrCreate(['nombre' => $category]);
-        }
+        collect($categories)
+            ->each(function ($category) {
+                CategoryModel::updateOrCreate(['nombre' => $category]);
+            });
     }
 }
